e0ea190717abf34e20596ba7a59e3d1da52e2fc8
Cloudox <cloudox@foxmail.com>
2016-05-04 22:56:18 +0800

    完成样式

--- SegmentControlDemo/ViewController.m
+++ SegmentControlDemo/ViewController.m
@@ -8,7 +8,19 @@
 
 #import "ViewController.h"
 
+//设备的宽高
+#define SCREENHEIGHT      [UIScreen mainScreen].bounds.size.height
+#define SCREENWIDTH       [UIScreen mainScreen].bounds.size.width
+//获取颜色
+#define RGBA(r, g, b, a)                    [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
+#define RGB(r, g, b)                        RGBA(r, g, b, 1.0f)
+#define cmGreen RGB(0x32, 0xAB, 0x64)
+//根据十六进制数来设置颜色
+#define kUIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
 @interface ViewController ()
+
+@property (nonatomic, strong) UISegmentedControl *segmentedControl;
 
 @end
 
@@ -16,12 +28,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"正在销售", @"已下架"]];
+    self.segmentedControl.frame = CGRectMake(-5, 50, SCREENWIDTH+10, 30);
+    self.segmentedControl.tintColor = cmGreen;
+    NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:cmGreen,NSForegroundColorAttributeName,[UIFont systemFontOfSize:18],NSFontAttributeName ,nil];
+    [self.segmentedControl setTitleTextAttributes:dic forState:UIControlStateNormal];
+    self.segmentedControl.selectedSegmentIndex = 0;
+    [self.segmentedControl addTarget:self action:@selector(selectItem:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.view addSubview:self.segmentedControl];
+    
+}
+
+- (void)selectItem:(UISegmentedControl *)sender {
+    if (sender.selectedSegmentIndex == 0) {
+        NSLog(@"正在销售");
+    } else {
+        NSLog(@"已下架");
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 @end
