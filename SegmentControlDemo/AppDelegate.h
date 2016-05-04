f0ca80304b5c97a775f272844149a8436ec0e120
Cloudox <cloudox@foxmail.com>
2016-05-04 21:58:49 +0800

    Initial Commit

---
+++ SegmentControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentControlDemo
+//
+//  Created by Cloudox on 16/5/4.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SegmentControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SegmentControlDemo
+//
+//  Created by Cloudox on 16/5/4.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
